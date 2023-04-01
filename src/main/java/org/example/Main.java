e80fb9a5623895b642ed23f84fdadb604a94e88f
qwikk <44299295+qwakk@users.noreply.github.com>
2023-04-01 14:44:42 +0200

    fixed typo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
         f1Data.setF1Data();
         f1Data.setNextRace();
 
-        File f = new File("tokens/t.token");
+        File f = new File("token/t.token");
         Scanner s = new Scanner(f);
 
         JDA bot = JDABuilder.createDefault(s.nextLine())
