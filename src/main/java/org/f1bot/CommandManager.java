96badcd0a719866f800c73511fb9c65ba8f6658c
qwikk <44299295+qwakk@users.noreply.github.com>
2023-04-20 16:34:54 +0200

    Fixed bot updating too often if last update was a long time ago

--- src/main/java/org/f1bot/CommandManager.java
+++ src/main/java/org/f1bot/CommandManager.java
@@ -31,7 +31,7 @@ public class CommandManager extends ListenerAdapter {
         if (lastUpdate.plusHours(2).isBefore(LocalDateTime.now())) {
             f1Data.update();
             System.out.println("UPDATING DATA");
-            lastUpdate = lastUpdate.plusHours(2);
+            lastUpdate = LocalDateTime.now();
         }
         if (event.getName().equals("ping")) {
             long ping = event.getTimeCreated().until(OffsetDateTime.now(), ChronoUnit.MILLIS);
@@ -39,7 +39,7 @@ public class CommandManager extends ListenerAdapter {
         }
         else if (event.getName().equals("getrace")) {
             int index = event.getOption("racenumber").getAsInt()-1;
-            if (!f1Data.hasRace(index)) {event.reply("invalid racenumber: "+index+1).queue();return;}
+            if (!f1Data.hasRace(index)) { event.reply("invalid racenumber: "+index+1).queue(); return; }
             Race race = f1Data.getRace(index);
             event.replyEmbeds(createRaceEmbed(race).build()).queue();
         }
