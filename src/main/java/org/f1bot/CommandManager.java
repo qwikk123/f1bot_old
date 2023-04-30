b9d4e5092fffe1c2fac608b278e400c90dec6814
qwikk <44299295+qwakk@users.noreply.github.com>
2023-04-30 16:58:03 +0200

    removed the need for errorchecking in /getrace

--- src/main/java/org/f1bot/CommandManager.java
+++ src/main/java/org/f1bot/CommandManager.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
@@ -37,7 +38,6 @@ public class CommandManager extends ListenerAdapter {
         }
         else if (event.getName().equals("getrace")) {
             int index = event.getOption("racenumber").getAsInt()-1;
-            if (!f1Data.hasRace(index)) { event.reply("invalid racenumber: "+(index+1)).queue(); return; }
             Race race = f1Data.getRace(index);
             event.replyEmbeds(EmbedCreator.createRace(race).build()).queue();
         }
@@ -56,8 +56,18 @@ public class CommandManager extends ListenerAdapter {
     @Override
     public void onGuildReady(GuildReadyEvent event) {
         List<CommandData> commandData = new ArrayList<>();
-        commandData.add(Commands.slash("ping", "check if don is sleeping :)"));
-        commandData.add(Commands.slash("getrace", "nth race").addOption(OptionType.INTEGER, "racenumber", "Race to get info from", true, true));
+        commandData.add(Commands.slash("ping", "ping the bot :)"));
+
+        OptionData option1 = new OptionData(
+                OptionType.INTEGER,
+                "racenumber",
+                "Race to get info from",
+                true,
+                true)
+                .setMinValue(1)
+                .setMaxValue(f1Data.raceList.size());
+
+        commandData.add(Commands.slash("getrace", "nth race").addOptions(option1));
         commandData.add(Commands.slash("nextrace", "Get the upcoming Grand Prix"));
         commandData.add(Commands.slash("driverstandings", "Get info on driver championship standings"));
         commandData.add(Commands.slash("constructorstandings", "Get info on constructor championship standings"));
