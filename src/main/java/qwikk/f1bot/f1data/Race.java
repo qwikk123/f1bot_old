168e063cd35f7bec82687acb652bbfc393569bc2
qwikk <44299295+qwakk@users.noreply.github.com>
2023-07-21 11:04:50 +0200

    changed upcoming messages to send at 07:00 instead of 14:00

--- src/main/java/qwikk/f1bot/f1data/Race.java
+++ src/main/java/qwikk/f1bot/f1data/Race.java
@@ -49,5 +49,5 @@ public class Race {
     public String getImageName() {
         return circuitName.replaceAll(" ","")+".png";
     }
-    public LocalDateTime getUpcomingDate() { return localDateTime.minusDays(2).withHour(14); }
+    public LocalDateTime getUpcomingDate() { return localDateTime.minusDays(2).withHour(7); }
 }
